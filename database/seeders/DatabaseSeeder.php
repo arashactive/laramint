92b455d6377321db9a9befbbefb0c9812b5f6afd
Arash Dehghani <arash.aspx@gmail.com>
2021-11-24 17:16:39 +0330

    create department migration and model and faker factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Database\Seeders;
-
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\Team::factory(10)->create();
+        \App\Models\Department::factory(10)->create();
     }
 }
