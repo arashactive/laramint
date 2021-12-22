bdf13f6856720cd8fe28e00076fede14083f04d2
Arash Dehghani <arash.aspx@gmail.com>
2021-12-22 13:47:22 +0330

    sidebar theme changed to success gradient

--- resources/views/layouts/menus/sidebar.blade.php
+++ resources/views/layouts/menus/sidebar.blade.php
@@ -1,12 +1,12 @@
 <!-- Sidebar -->
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+<ul class="navbar-nav bg-gradient-success sidebar sidebar-dark accordion" id="accordionSidebar">
 
     <!-- Sidebar - Brand -->
     <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">SB Admin <sup>2</sup></div>
+        <div class="sidebar-brand-text mx-3">Mint <sup>V 0.01</sup></div>
     </a>
 
     <!-- Divider -->
