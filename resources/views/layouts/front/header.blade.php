702955cb09e83f8e0bd55666418baa21ffb1b27d
Arash Dehghani <arash.aspx@gmail.com>
2022-02-08 15:15:56 -0800

    title of front template is changed

--- resources/views/layouts/front/header.blade.php
+++ resources/views/layouts/front/header.blade.php
@@ -1,6 +1,6 @@
 <head>
     <meta charset="utf-8">
-    <title>ShabaniFard Ha :D LaraMint</title>
+    <title>LaraMint eLearning</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
