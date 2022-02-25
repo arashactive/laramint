aa8a6acb4fade423cec5200c64885bdad58e6c34
Arash Dehghani <arash.aspx@gmail.com>
2022-02-25 11:23:23 -0800

    redirect home to login

--- app/Http/Controllers/Front/FrontController.php
+++ app/Http/Controllers/Front/FrontController.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\Controller;
 class FrontController extends Controller
 {
     public function index(){
-        return view('contents.front.index.welcome');
+        return redirect(route('login'));
+        //return view('contents.front.index.welcome');
     }
 }
