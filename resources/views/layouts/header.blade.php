bd1cc047f5ff4096a2f0cd56f3756c28ac010972
Arash Dehghani <arash.aspx@gmail.com>
2022-03-04 13:58:51 -0800

    readme demo link is updated

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -18,5 +18,12 @@
     <link href="{{ URL::to('css/sb-admin-2.css') }}" rel="stylesheet">
     <link href="{{ URL::to('css/custom.css') }}" rel="stylesheet">
     <link href="{{ URL::to('vendor/jQuery-TE/jquery-te-1.4.0.css') }}" rel="stylesheet">
-
+    <link rel="profile" href="http://gmpg.org/xfn/11">
+    <!-- Google Analytics opt-out snippet added by Site Kit -->
+    <script type="text/javascript">
+    window["ga-disable-UA-118287266-1"] = true;
+    </script>
+    <script type="text/javascript">
+    window["ga-disable-G-JE49KMQSTV"] = true;
+    </script>
 </head>
