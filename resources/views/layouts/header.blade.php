99e3fb1640eb3d718a55b4967829c3b7013b9848
Arash Dehghani <arash.aspx@gmail.com>
2022-04-06 00:25:41 +0430

    darkmode available

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -16,6 +16,7 @@
 
     <!-- Custom styles for this template-->
     <link href="{{ URL::to('css/sb-admin-2.css') }}" rel="stylesheet">
+    
     <link href="{{ URL::to('css/custom.css') }}" rel="stylesheet">
     <link href="{{ URL::to('vendor/jQuery-TE/jquery-te-1.4.0.css') }}" rel="stylesheet">
     <link rel="profile" href="http://gmpg.org/xfn/11">
