4b7cd3f4416489abf446d507dc4d90696584349f
Arash Dehghani <arash.aspx@gmail.com>
2022-04-24 13:59:40 +0430

    fix width

--- resources/views/components/atoms/progress.blade.php
+++ resources/views/components/atoms/progress.blade.php
@@ -1,8 +1,8 @@
 <div class="progress">
     <div class="progress-bar {{ $color }}" 
         role="progressbar" 
-        style="width: {{ $width ?: '' }}%" 
-        aria-valuenow="{{ $width ?: '' }}" 
+        style="width: 0%" 
+        aria-valuenow="0" 
         aria-valuemin="0" 
         aria-valuemax="100">
     </div>
