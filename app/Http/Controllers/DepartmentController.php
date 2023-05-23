12324111c9a17f3a39520fb1fa4583426c343327
Arash Dehghani <arash.aspx@gmail.com>
2023-05-24 02:35:02 +0330

    departmentController updated

--- app/Http/Controllers/DepartmentController.php
+++ app/Http/Controllers/DepartmentController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\DepartmentRequest;
-use App\Models\Department;
 use App\Services\Back\Educations\DepartmentAdminService;
 
 class DepartmentController extends Controller
