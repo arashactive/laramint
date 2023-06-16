e82c0fe3749f1d9a608b2560c366461ef62a47c1
shriradharani <brij.phpdev@gmail.com>
2023-06-16 11:38:39 +0530

    remove close button from popup - to safely redirectthe student to login

--- resources/views/contents/landing_pages/index.blade.php
+++ resources/views/contents/landing_pages/index.blade.php
@@ -540,7 +540,7 @@ $referrer_url = $_SERVER['HTTP_REFERER'] ?? '';
       <!-- Modal Header -->
       <div class="modal-header">
         <h4 class="modal-title">Redirecting to login</h4>
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <!--<button type="button" class="close" data-dismiss="modal">&times;</button>-->
       </div>
 
       <!-- Modal body -->
