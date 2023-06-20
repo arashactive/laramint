0b1b55cbb11fb2e9877f12e3e7f9855ca4f8492e
shriradharani <brij.phpdev@gmail.com>
2023-06-20 09:34:42 +0530

    removed extra content

--- resources/views/contents/front/index/about.blade.php
+++ resources/views/contents/front/index/about.blade.php
@@ -8,46 +8,6 @@
 <!-- About Start -->
 <div class="container-xxl py-5">
     <div class="container py-5 px-lg-5">
-        <div class="row g-5 align-items-center">
-            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
-                <!--<p class="section-title text-secondary">About Us<span></span></p>-->
-                <h1 class="mb-5">About Us</h1>
-
-                <div class="skill mb-4">
-                    <div class="d-flex justify-content-between">
-                        <p class="mb-2">Thousands of video examples of language used in real life</p>
-                        <p class="mb-2">85%</p>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar bg-primary" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-                <div class="skill mb-4">
-                    <div class="d-flex justify-content-between">
-                        <p class="mb-2">Immersive learning that makes you feel like you're right there with the locals</p>
-                        <p class="mb-2">90%</p>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar bg-secondary" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-                <div class="skill mb-4">
-                    <div class="d-flex justify-content-between">
-                        <p class="mb-2">Gamified tests that train your language skills</p>
-                        <p class="mb-2">95%</p>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar bg-dark" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-                <a href="" class="btn btn-primary py-sm-3 px-sm-5 rounded-pill mt-3">Read More</a>
-            </div>
-            <div class="col-lg-6">
-                <img class="img-fluid wow zoomIn" data-wow-delay="0.5s" src="front/img/about.png">
-            </div>
-        </div>
-    </div>
-    <div class="container py-5 px-lg-5">
         <div class="row align-items-center">
             <h1 class="text-center mb-5">A Message from Director's desk</h1>
             <div class="col-12 wow fadeInUp" data-wow-delay="0.6s">
